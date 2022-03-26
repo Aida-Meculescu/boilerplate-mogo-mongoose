45faeb2f4ebbb66a9c8c960804997a3801c4082f
Aida-Meculescu <aida.meculescu@gmail.com>
2022-03-26 21:40:11 +0200

    Delete One Document

--- myApp.js
+++ myApp.js
@@ -85,7 +85,11 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+
+  Person.findByIdAndRemove(personId, (err, personDeleted) => {
+    if (err) return console.error(err);
+    done(null, personDeleted)
+  })
 };
 
 const removeManyPeople = (done) => {
